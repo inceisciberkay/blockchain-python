97564b6df8745b2806a3b7e8c34ee752b87c6168
Berkay Inceisci <inceisciberkay@gmail.com>
2023-12-08 23:28:27 +0300

    add signal handling logic for backgroung process

--- src/node.py
+++ src/node.py
@@ -1,9 +1,19 @@
 import argparse
 import time
+import sys
+import signal
+
+def signal_handler(signum, frame):
+    # do something
+    sys.exit(0)
 
 def background_task(process_id: str):
     # do something
-    time.sleep(1)
+    signal.signal(signal.SIGTERM, signal_handler)
+    node_name = process_id.split('_')
+
+    while True: 
+        time.sleep(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run a background task with a specific process ID.")
